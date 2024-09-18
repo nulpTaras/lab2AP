7c11000c84d5930739977eb18a0b9724413d4ed9
nulpTaras <taras.neridnyi.ri.2024@lpnu.ua>
2024-09-19 00:44:32 +0300

    onlycout

--- lab.cpp
+++ lab.cpp
@@ -8,16 +8,7 @@
 using namespace std;
 int main()
 {
-    double x; // вхідний параметр
-    double z1; // результат обчислення 1-го виразу
-    double z2; // результат обчислення 2-го виразу
-    cout << "x = "; cin >> x;
-    z1 = ( sin(2*x) + sin(5*x) - sin(3*x) )  /  ( cos(x) + 1 - (2 * ( sin(2*x) * sin(2*x) )) );
-    z2 = 2 * sin(x);
-    cout << endl;
-    cout << "z1 = " << z1 << endl;
-    cout << "z2 = " << z2 << endl;
-    cin.get();
+
 
     cout << "Change 1,2" << endl;
     cout << "Change 1,2" << endl;
